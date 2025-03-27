314c37a2d47cbdcd9cd36abc8d365d35349714b0
Newton Duarte <newton_@hotmail.com>
2025-03-27 16:19:36 -0300

    feat: add sign in page and module initial structure

---
+++ src/app/[lang]/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import SignInModule from '@/features/auth/sign-in'
+import { getDictionary } from '@/features/i18n/get-dictionaries'
+
+export default async function SignInPage({ params }: NextLangParams) {
+  const { lang } = await params
+  const dict = await getDictionary(lang)
+
+  return <SignInModule dictionary={dict} />
+}
---
+++ src/features/auth/sign-in/index.tsx
@@ -0,0 +1,36 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { getDictionary } from '@/features/i18n/get-dictionaries'
+
+import { SignInForm } from '../components/sign-in/sign-in-form'
+
+type SignInModulePropsProps = {
+  dictionary: Awaited<ReturnType<typeof getDictionary>>['auth']
+}
+
+export default async function SignInModule({
+  dictionary,
+}: SignInModulePropsProps) {
+  return (
+    <main className="flex h-screen flex-col items-center justify-center p-4 sm:p-0">
+      <Card className="min-w-full sm:min-w-md">
+        <CardHeader className="p-4 text-center sm:p-6">
+          <CardTitle className="text-2xl">
+            {dictionary.auth['sign-in'].title}
+          </CardTitle>
+          <CardDescription>
+            {dictionary.auth['sign-in'].subtitle}
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="p-4 sm:p-6">
+          <SignInForm dictionary={dictionary} />
+        </CardContent>
+      </Card>
+    </main>
+  )
+}
