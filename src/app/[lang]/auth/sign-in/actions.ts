1eda39bd4641da8b01e70ae687261bac489690ff
Newton Duarte <newton_@hotmail.com>
2025-03-28 16:08:47 -0300

    feat: add validation to sign in form

--- src/app/[lang]/auth/sign-in/actions.ts
+++ src/app/[lang]/auth/sign-in/actions.ts
@@ -1,14 +1,57 @@
 'use server'
 
+import { HTTPError } from 'ky'
+import { z } from 'zod'
+
 import { signInWithEmailAndPassword } from '@/http/sign-in-with-email-and-password'
 
-export async function signInAction(previousState: unknown, data: FormData) {
-  const { email, password } = Object.fromEntries(data)
+interface SignInActionResponse {
+  success: boolean
+  message: string | null
+  errors: {
+    email?: string[] | undefined
+    password?: string[] | undefined
+  } | null
+}
 
-  const response = await signInWithEmailAndPassword({
-    email: String(email),
-    password: String(password),
-  })
+const signInSchema = z.object({
+  email: z.string().email({ message: 'Please provide a valid email' }),
+  password: z.string().min(6, 'Please provide your password'),
+})
 
-  return response
+export async function signInAction(
+  previousState: unknown,
+  data: FormData
+): Promise<SignInActionResponse> {
+  const result = signInSchema.safeParse(Object.fromEntries(data))
+
+  if (!result.success) {
+    const errors = result.error.flatten().fieldErrors
+
+    return { success: false, message: 'Error', errors }
+  }
+
+  const { email, password } = result.data
+
+  try {
+    const response = await signInWithEmailAndPassword({
+      email,
+      password,
+    })
+  } catch (error) {
+    if (error instanceof HTTPError) {
+      const { message } = await error.response.json()
+      return { success: false, message, errors: null }
+    }
+
+    console.error(error)
+
+    return {
+      success: false,
+      message: 'Unexpected error, try again.',
+      errors: null,
+    }
+  }
+
+  return { success: true, message: 'Success', errors: null }
 }
--- src/features/auth/components/sign-in/sign-in-form.tsx
+++ src/features/auth/components/sign-in/sign-in-form.tsx
@@ -1,10 +1,12 @@
 'use client'
 
-import { Loader2, LogIn } from 'lucide-react'
+import { AlertTriangle, Loader2, LogIn } from 'lucide-react'
 import Link from 'next/link'
 import { useActionState } from 'react'
 
 import { signInAction } from '@/app/[lang]/auth/sign-in/actions'
+import { FieldError } from '@/components/common/form/field-error'
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
@@ -15,12 +17,26 @@ type SignInFormProps = {
 }
 
 export function SignInForm({ dictionary }: SignInFormProps) {
-  // eslint-disable-next-line unicorn/no-null
-  const [state, formAction, isPending] = useActionState(signInAction, null)
+  const [{ success, message, errors }, formAction, isPending] = useActionState(
+    signInAction,
+    {
+      success: false,
+      message: null,
+      errors: null,
+    }
+  )
 
   return (
     <form action={formAction} className="space-y-4">
-      <h1 className="text-center text-sm font-bold text-primary">{state}</h1>
+      {!success && message && (
+        <Alert variant="destructive">
+          <AlertTriangle className="size-4" />
+          <AlertTitle>Sign in failed!</AlertTitle>
+          <AlertDescription>
+            <p>{message}</p>
+          </AlertDescription>
+        </Alert>
+      )}
       <div className="space-y-2">
         <Label htmlFor="email">{dictionary.auth.common.email}</Label>
         <Input
@@ -31,6 +47,7 @@ export function SignInForm({ dictionary }: SignInFormProps) {
           className="h-10"
           autoComplete="email"
         />
+        {errors?.email && <FieldError>{errors.email[0]}</FieldError>}
       </div>
       <div className="space-y-2">
         <Label htmlFor="password">{dictionary.auth.common.password}</Label>
@@ -42,6 +59,7 @@ export function SignInForm({ dictionary }: SignInFormProps) {
           className="h-10"
           autoComplete="current-password"
         />
+        {errors?.password && <FieldError>{errors.password[0]}</FieldError>}
       </div>
       <div className="flex flex-col gap-4">
         <Button className="w-full" size="lg" disabled={isPending}>
