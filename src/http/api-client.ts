7bace37e95bde884d63a5e5b374ee2cf370aec5b
Newton Duarte <newton_@hotmail.com>
2025-03-28 17:32:01 -0300

    feat: add ky hook to send bearer token on requests when the token is on cookies

--- src/http/api-client.ts
+++ src/http/api-client.ts
@@ -1,5 +1,26 @@
+import { CookiesFn, getCookie } from 'cookies-next'
 import ky from 'ky'
+
+import { AUTH_TOKEN_KEY } from '@/features/auth/constants'
 
 export const api = ky.create({
   prefixUrl: 'http://localhost:8080',
+  hooks: {
+    beforeRequest: [
+      async (request) => {
+        let cookieStore: CookiesFn | undefined
+
+        if (typeof globalThis === 'undefined') {
+          const { cookies: serverCookies } = await import('next/headers')
+          cookieStore = serverCookies
+        }
+
+        const token = await getCookie(AUTH_TOKEN_KEY, { cookies: cookieStore })
+
+        if (token) {
+          request.headers.set('Authorization', `Bearer ${token}`)
+        }
+      },
+    ],
+  },
 })
