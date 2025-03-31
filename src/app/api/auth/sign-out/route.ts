a46f2ad74ae9fef824a578231d76565dc779d9d6
Newton Duarte <newton_@hotmail.com>
2025-03-31 18:13:12 -0300

    fix: sign out api route by passing the locale on the search params

--- src/app/api/auth/sign-out/route.ts
+++ src/app/api/auth/sign-out/route.ts
@@ -2,10 +2,16 @@ import { cookies } from 'next/headers'
 import { NextRequest, NextResponse } from 'next/server'
 
 import { AUTH_TOKEN_KEY } from '@/features/auth/constants'
+import { i18n } from '@/features/i18n/i18n-config'
 
 export async function GET(request: NextRequest) {
   const redirectUrl = request.nextUrl.clone()
-  redirectUrl.pathname = '/auth/sign-in'
+  const searchParams = request.nextUrl.searchParams
+  const currentLocale = searchParams.get('locale') ?? i18n.defaultLocale
+  redirectUrl.pathname = `${currentLocale}/auth/sign-in`
+  redirectUrl.search = ''
+
+  console.log('SIGN OUT!')
 
   const nextjsCookies = await cookies()
   nextjsCookies.delete(AUTH_TOKEN_KEY)
