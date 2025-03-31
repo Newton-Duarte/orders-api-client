3dc0a1e04bdc94d407c12570551e8002c3e5f459
Newton Duarte <newton_@hotmail.com>
2025-03-31 18:14:41 -0300

    feat: add app layout and home page

--- src/app/[lang]/(app)/layout.tsx
+++ src/app/[lang]/(app)/layout.tsx
@@ -1,24 +1,31 @@
+import { redirect } from 'next/navigation'
+import { ReactNode } from 'react'
+
 import { Footer } from '@/components/footer'
 import { Header } from '@/components/header'
-import { auth } from '@/features/auth/auth'
+import { isAuthenticated } from '@/features/auth/auth'
 import { getDictionary } from '@/features/i18n/get-dictionaries'
 
-export default async function Home({
+export default async function AppLayout({
+  children,
   params,
-}: {
+}: Readonly<{
+  children: ReactNode
   params: Promise<{ lang: string }>
-}) {
+}>) {
   const { lang } = await params
   const dict = await getDictionary(lang)
 
-  const user = await auth()
+  if (!isAuthenticated()) {
+    redirect('/auth/sign-in')
+  }
 
   return (
     <>
-      <Header lang={lang} dictionary={dict} user={user} />
-      <main className="container mx-auto h-[calc(100vh-164px)] bg-background">
+      <Header lang={lang} />
+      <main className="container mx-auto h-[calc(100vh-164px)]">
         <h1>{dict.landing.welcome}</h1>
-        <pre>{JSON.stringify(user, null, 2)}</pre>
+        {children}
       </main>
       <Footer dictionary={dict.footer} />
     </>
