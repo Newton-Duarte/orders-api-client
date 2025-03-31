1a2953ea567505590d4192ebcfc8863fe7c84f51
Newton Duarte <newton_@hotmail.com>
2025-03-31 15:51:02 -0300

    feat: add language switcher component

---
+++ src/components/language-switcher.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import { Globe } from 'lucide-react'
+import { usePathname, useRouter } from 'next/navigation'
+
+import { getDictionary } from '@/features/i18n/get-dictionaries'
+import { i18n, languageNames } from '@/features/i18n/i18n-config'
+
+import { Button } from './ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+
+type LanguageSwitcherProps = {
+  currentLocale: string
+  dictionary: Awaited<ReturnType<typeof getDictionary>>['language-switcher']
+}
+
+export function LanguageSwitcher({
+  currentLocale,
+  dictionary,
+}: LanguageSwitcherProps) {
+  const router = useRouter()
+  const pathname = usePathname()
+
+  const handleChangeLanguage = (locale: string) => {
+    const pathWithoutLocale = pathname.replace(/^\/(en-US|pt-BR)/, '') || '/'
+    router.push(`/${locale}${pathWithoutLocale}`)
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline">
+          <Globe className="size-4" />
+          {languageNames[currentLocale]}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>
+          {dictionary['language-switcher']['select-language']}
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        {i18n.locales.map((locale) => (
+          <DropdownMenuItem
+            key={locale}
+            onClick={() => handleChangeLanguage(locale)}
+            className={currentLocale === locale ? 'bg-muted' : ''}
+          >
+            {languageNames[locale] || locale}
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
