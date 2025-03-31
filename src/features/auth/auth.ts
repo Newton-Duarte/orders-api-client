660dce561fd49cfd76134f6bdf82aab972f77299
Newton Duarte <newton_@hotmail.com>
2025-03-31 18:15:11 -0300

    chore: add todo to fix it later

--- src/features/auth/auth.ts
+++ src/features/auth/auth.ts
@@ -14,6 +14,11 @@ export async function auth() {
   const nextjsCookies = await cookies()
   const token = nextjsCookies.get(AUTH_TOKEN_KEY)?.value
 
+  // TODO: Need to fix a bug where when the user tries to log in after a log out
+  // the token is being undefined (I think is something related to the AppLayout)
+  // Could be next cache too
+  console.log('auth token', token)
+
   if (!token) {
     redirect('/auth/sign-in')
   }
