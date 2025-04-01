d5ebc5b637d5f1db3bb966417542b9683e0545c1
Newton Duarte <newton_@hotmail.com>
2025-04-01 10:49:06 -0300

    refactor: remove locale detect on sign out

--- src/app/api/auth/sign-out/route.ts
+++ src/app/api/auth/sign-out/route.ts
@@ -2,16 +2,10 @@ import { cookies } from 'next/headers'
 import { NextRequest, NextResponse } from 'next/server'
 
 import { AUTH_TOKEN_KEY } from '@/features/auth/constants'
-import { i18n } from '@/features/i18n/i18n-config'
 
 export async function GET(request: NextRequest) {
   const redirectUrl = request.nextUrl.clone()
-  const searchParams = request.nextUrl.searchParams
-  const currentLocale = searchParams.get('locale') ?? i18n.defaultLocale
-  redirectUrl.pathname = `${currentLocale}/auth/sign-in`
-  redirectUrl.search = ''
-
-  console.log('SIGN OUT!')
+  redirectUrl.pathname = '/auth/sign-in'
 
   const nextjsCookies = await cookies()
   nextjsCookies.delete(AUTH_TOKEN_KEY)
