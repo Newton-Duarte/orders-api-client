89035e0c306def731411a87d39658e1b5c3fd8d1
Newton Duarte <newton_@hotmail.com>
2025-04-01 21:45:46 -0300

    feat: add home page initial structure

--- src/app/[lang]/(app)/page.tsx
+++ src/app/[lang]/(app)/page.tsx
@@ -1,3 +1,20 @@
-export default async function HomePage() {
-  return <div>Home Page</div>
+import { ListHeader } from '@/components/common/list-header'
+import { getDictionary } from '@/features/i18n/get-dictionaries'
+
+export default async function HomePage({
+  params,
+}: Readonly<{
+  params: Promise<{ lang: string }>
+}>) {
+  const { lang } = await params
+  const dict = await getDictionary(lang)
+
+  return (
+    <div>
+      <ListHeader
+        title={dict.dashboard.title}
+        description={dict.dashboard.description}
+      />
+    </div>
+  )
 }
