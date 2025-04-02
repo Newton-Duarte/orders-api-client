c43c55b4c2632b07ef6f6ed734edf1576d10a122
Newton Duarte <newton_@hotmail.com>
2025-04-01 21:46:13 -0300

    feat: add orders page initial structure

--- src/app/[lang]/(app)/orders/page.tsx
+++ src/app/[lang]/(app)/orders/page.tsx
@@ -1,3 +1,27 @@
-export default function OrdersPage() {
-  return <div>OrdersPage</div>
+import { Plus } from 'lucide-react'
+
+import { ListHeader } from '@/components/common/list-header'
+import { Button } from '@/components/ui/button'
+import { getDictionary } from '@/features/i18n/get-dictionaries'
+
+export default async function OrdersPage({
+  params,
+}: Readonly<{
+  params: Promise<{ lang: string }>
+}>) {
+  const { lang } = await params
+  const dict = await getDictionary(lang)
+
+  return (
+    <div className="flex items-center justify-between">
+      <ListHeader
+        title={dict.orders.title}
+        description={dict.orders.description}
+      />
+      <Button>
+        <Plus className="size-4" />
+        {dict.orders['add-order']}
+      </Button>
+    </div>
+  )
 }
