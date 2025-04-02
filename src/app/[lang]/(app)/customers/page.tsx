6cd58e41c6675272b427028996c3b08af66e0031
Newton Duarte <newton_@hotmail.com>
2025-04-01 21:46:08 -0300

    feat: add customers page initial structure

--- src/app/[lang]/(app)/customers/page.tsx
+++ src/app/[lang]/(app)/customers/page.tsx
@@ -1,3 +1,27 @@
-export default function CustomersPage() {
-  return <div>CustomersPage</div>
+import { Plus } from 'lucide-react'
+
+import { ListHeader } from '@/components/common/list-header'
+import { Button } from '@/components/ui/button'
+import { getDictionary } from '@/features/i18n/get-dictionaries'
+
+export default async function CustomersPage({
+  params,
+}: Readonly<{
+  params: Promise<{ lang: string }>
+}>) {
+  const { lang } = await params
+  const dict = await getDictionary(lang)
+
+  return (
+    <div className="flex items-center justify-between">
+      <ListHeader
+        title={dict.customers.title}
+        description={dict.customers.description}
+      />
+      <Button>
+        <Plus className="size-4" />
+        {dict.customers['add-customer']}
+      </Button>
+    </div>
+  )
 }
