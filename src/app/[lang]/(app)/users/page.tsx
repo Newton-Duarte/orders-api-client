a05922950276d24e2ec5150fe6ea49b16c2e106d
Newton Duarte <newton_@hotmail.com>
2025-04-01 21:45:55 -0300

    feat: add users page initial structure

--- src/app/[lang]/(app)/users/page.tsx
+++ src/app/[lang]/(app)/users/page.tsx
@@ -1,3 +1,27 @@
-export default function UsersPage() {
-  return <div>UsersPage</div>
+import { Plus } from 'lucide-react'
+
+import { ListHeader } from '@/components/common/list-header'
+import { Button } from '@/components/ui/button'
+import { getDictionary } from '@/features/i18n/get-dictionaries'
+
+export default async function UsersPage({
+  params,
+}: Readonly<{
+  params: Promise<{ lang: string }>
+}>) {
+  const { lang } = await params
+  const dict = await getDictionary(lang)
+
+  return (
+    <div className="flex items-center justify-between">
+      <ListHeader
+        title={dict.users.title}
+        description={dict.users.description}
+      />
+      <Button>
+        <Plus className="size-4" />
+        {dict.users['add-user']}
+      </Button>
+    </div>
+  )
 }
